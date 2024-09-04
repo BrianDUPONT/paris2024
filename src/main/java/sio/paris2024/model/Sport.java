b11b4fd25c1866e93a1141c2faffec4dd92cee33
brian <brian-dupont@hotmail.com>
2024-09-04 11:27:32 +0200

    Création de la classe Sport

---
+++ src/main/java/sio/paris2024/model/Sport.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sio.paris2024.model;
+
+/**
+ *
+ * @author SIO2
+ */
+public class Sport {
+    
+}
