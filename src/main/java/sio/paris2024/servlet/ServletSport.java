3ecf8519d7e89cedfce86dd3d8c43d9f4b2063aa
brian <brian-dupont@hotmail.com>
2024-09-04 12:06:09 +0200

    Ajout de la connection à la bdd

--- src/main/java/sio/paris2024/servlet/ServletSport.java
+++ src/main/java/sio/paris2024/servlet/ServletSport.java
@@ -10,12 +10,16 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.sql.Connection;
 
 /**
  *
  * @author SIO2
  */
+
+
 public class ServletSport extends HttpServlet {
+    Connection cnx ;
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
