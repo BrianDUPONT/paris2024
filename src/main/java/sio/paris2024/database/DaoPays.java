23938b2207e39140d15b0d9a1b467eb6e71d242e
brian <brian-dupont@hotmail.com>
2024-09-09 10:52:54 +0200

    Ajout de SetCode et de GetPaysById

--- src/main/java/sio/paris2024/database/DaoPays.java
+++ src/main/java/sio/paris2024/database/DaoPays.java
@@ -36,6 +36,7 @@ public class DaoPays {
                 
                 Pays p = new Pays();
                 p.setId(resultatRequete.getInt("id"));
+                p.setCode(resultatRequete.getString("code"));
                 p.setNom(resultatRequete.getString("nom"));
                 
                 lesPays.add(p);
@@ -47,6 +48,40 @@ public class DaoPays {
             System.out.println("La requête de getLespayss e généré une erreur");
         }
         return lesPays;
+    }
+        
+    public static Pays getPaysById(Connection cnx, int idPays){
+        
+        Athlete a = new Athlete();
+        try{
+            requeteSql = cnx.prepareStatement("select p.id as p_id, p.code as p_code, p.nom as p_nom,  a.id as a_id, a.nom as a_nom " +
+                         " from pays p inner join athelete a " +
+                         " on p.id = a.pays_id " + 
+                         " where p.id = ? ");
+            //System.out.println("REQ="+ requeteSql);
+            requeteSql.setInt(1, idPays);
+            resultatRequete = requeteSql.executeQuery();
+            
+            if (resultatRequete.next()){
+                
+                   a.setId(resultatRequete.getInt("p_id"));
+                   a.setCode(resultatRequete.getString("p_nom"));
+                    
+                   Pays p = new Pays();
+                   p.setId(resultatRequete.getInt("p_id"));
+                   p.setCode(resultatRequete.getString("p_nom"));
+                
+                    a.setPays(p);
+                
+            }
+           
+        }
+        catch (SQLException e){
+            e.printStackTrace();
+            System.out.println("La requête de getLesPompiers e généré une erreur");
+        }
+        return a;
+    }
         
     }
     
